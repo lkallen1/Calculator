63b96f8b70b43cbe22c26062f15dd410b6200edc
Lloyd Allen <lkallen1@asu.edu>
2016-02-20 17:03:26 -0700

    Initial check-in of Calculator and CalculatorTest

---
+++ Assign3S16/src/cse360assign3/Calculator.java
@@ -0,0 +1,91 @@
+package cse360assign3;
+
+/**
+ * Class to create a simple calculator.
+ * 
+ * @author Lloyd Allen PIN 106 for CSE360 Spring 2016
+ *
+ * @version Feb 20, 2016
+ * 
+ */
+
+public class Calculator {
+
+	private int total;
+
+	/**
+	 * Creates a calculator, total is zero by default
+	 * 
+	 * @param none
+	 */
+
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 * getTotal - returns the current total
+	 * 
+	 * @param none
+	 * 
+	 * @return total
+	 */
+
+	public int getTotal () {
+		return 0;
+	}
+
+	/**
+	 * add - add a value to the total
+	 * 
+	 * @param value	value to add
+	 */
+
+	public void add (int value) {
+		
+	}
+
+	/**
+	 * subtract - subtract a value from the total
+	 * 
+	 * @param value	value to subtract
+	 */
+
+	public void subtract (int value) {
+		
+	}
+
+	/**
+	 * multiply - multiply a value with the total
+	 * 
+	 * @param value	value to multiply
+	 */
+
+	public void multiply (int value) {
+		
+	}
+
+	/**
+	 * divide - divide the total by a value
+	 * 
+	 * @param value	value to divide
+	 */
+
+	public void divide (int value) {
+		
+	}
+
+	/**
+	 * getHistory - returns a String that contains a history
+	 *              of all recent calculations completed
+	 * 
+	 * @param none
+	 * 
+	 * @return string containing all values and operations
+	 *                used to compute the current total
+	 */
+
+	public String getHistory () {
+		return "";
+	}
+}
